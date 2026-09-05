b6563cd451ab24a0adf0253900c6b7e5ab2b6db2
hexiongwu1995_local <hexiongwu1995@outlook.com>
2026-09-05 22:03:03 +0800

    修改style.css，屏幕宽度小于700px时，隐藏.lil-gui

--- examples/itemplate/main.js
+++ examples/itemplate/main.js
@@ -72,18 +72,6 @@ let eventObj = {
 };
 
 const gui = new GUI({ container: wrapper });
-// gui.domElement.style.cssText = `
-// position: absolute; 
-// top: 0px; 
-// right: 0px; 
-// z-index: 10;
-// --background-color: #dddddd;
-// --title-background-color: #cccccc;
-// --title-text-color: #000000;
-// --widget-color: #cccccc;
-// --hover-color: #bbbbbb;
-// --text-color: #000000;
-// `;
 
 gui.title("Controls");
 gui.add(eventObj, "FullScreen");
